b916bdfb4375d13f30fee62119878cd7c434e770
RafaelSilvaSi <rafaelkaperski@gmail.com>
2021-04-27 17:03:23 -0300

    0.0.1

--- src/database/index.js
+++ src/database/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const authConfig = require('../config/auth.json');
 
 mongoose.connect(authConfig.database
-  , { useMongoClient: true });
+  , { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
