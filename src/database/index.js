11c0ba441c93c826de7ceea506ef41bbf78b9c63
RafaelSilvaSi <rafaelkaperski@gmail.com>
2021-04-27 16:09:48 -0300

    procfile

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const authConfig = require('../../config/auth');
+const authConfig = require('../config/auth.json');
 
 mongoose.connect(authConfig.database
   , { useMongoClient: true });
