a87d51c82799a15699a51ea49209580bd03a20fb
RafaelSilvaSi <rafaelkaperski@gmail.com>
2021-04-27 21:04:39 -0300

    0.1.0

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,8 @@
+
 const mongoose = require('mongoose');
 
 
-mongoose.connect( process.env.MONGO_URL
-  , { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URL , { useNewUrlParser: true, useUnifiedTopology: true  });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
